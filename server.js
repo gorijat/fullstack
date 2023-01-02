d5a17f040ca17e351d6b8010071e8d5a1cd85790
Goran Gajic <you@example.com>
2023-01-02 20:02:52 +0100

    forth commit

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongojs = require('mongojs');
+const db = mongojs('fullstack',['users']);
 const app = express();
 
 app.use(express.json());
@@ -9,13 +11,23 @@ app.get('/data',(req,res)=>{
 
 app.post('/register',(req,res)=>{
     // save new user
-   res.send('ok')
+   db.users.insert({name:req.body.name,pass:req.body.pass},(err,docs)=>{
+    res.send('ok')
+   })
+   
 })
 
 app.post('/login',(req,res)=>{
     // find user from db
-    console.log(req.body);
-    res.send('ok')
+    db.users.find({name:req.body.username,pass:req.body.password},(err,docs)=>{
+        if (docs.length == 1) {
+            res.send({
+                name: docs[0].name,
+                token: docs[0]._id
+            })
+        }
+    })
+    
 })
 
 app.listen(9000,()=>{
